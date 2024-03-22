e8f13e5df5ba5e1b0c364e5d633050ac98189c18
dzehil02 <dzehil02@gmail.com>
2024-03-22 22:44:27 +0100

    feat: add hash password using bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,6 +7,11 @@ import { checkEntityById } from 'src/utils/modelValidators';
 import { transformUserResponse, transformUsersResponse } from 'src/utils/transformUsersResponse';
 import { User } from 'src/types/types';
 import { User as UserPrismaClient } from '@prisma/client';
+import { config } from 'dotenv';
+import { compare, hash } from 'bcrypt';
+
+config();
+const CRYPT_SALT = Number(process.env.CRYPT_SALT) || 10;
 
 @Injectable()
 export class UserService {
@@ -20,10 +25,15 @@ export class UserService {
         return checkEntityById(id, user);
     }
     async create(createUserDto: CreateUserDto): Promise<Omit<User, 'password'>> {
+        const { login, password } = createUserDto;
+        const hashedPassword = await this.hashPassword(password);
+
         const newUser = {
             id: createPassword(),
-            ...createUserDto,
+            login,
+            password: hashedPassword,
         };
+
         const resUser = await this.prismaService.user.create({ data: newUser });
         return transformUserResponse(resUser);
     }
@@ -41,16 +51,25 @@ export class UserService {
 
     async update(id: string, updatePasswordDto: UpdatePasswordDto): Promise<Omit<User, 'password'>> {
         const user = await this.getUser(id);
-        if (user.password !== updatePasswordDto.oldPassword) {
+
+        const { password } = user;
+        const { oldPassword, newPassword } = updatePasswordDto;
+        const passwordEquals = await compare(oldPassword, password);
+
+        if (!passwordEquals) {
             throw new ForbiddenException('Old password is wrong');
         }
+
+        const hashedNewPassword = await this.hashPassword(newPassword);
+
         const updatedUser = await this.prismaService.user.update({
             where: { id },
             data: {
-                password: updatePasswordDto.newPassword,
+                password: hashedNewPassword,
                 version: user.version + 1,
             },
         });
+
         const resUser = { ...updatedUser };
         return transformUserResponse(resUser);
     }
@@ -58,5 +77,9 @@ export class UserService {
     async remove(id: string): Promise<void> {
         const user = await this.getUser(id);
         await this.prismaService.user.delete({ where: { id: user.id } });
+    }
+
+    private hashPassword(password: string): Promise<string> {
+        return hash(password, CRYPT_SALT);
     }
 }
